1b6d83a76b867dd79b533e83857504f9ec2f732d
Christopher Hlubek <hlubek@networkteam.com>
2010-10-18 13:24:33 +0200

    [~TASK] CouchDB: More documentation for the client

--- Classes/Client.php
+++ Classes/Client.php
@@ -39,6 +39,14 @@ class Client {
 	 */
 	protected $database;
 
+	/**
+	 * Create a new CouchDB client
+	 *
+	 * @param string $dataSourceName The CouchDB connection parameters as URL, e.g. http://user:pass@127.0.0.1:5984
+	 * @param array $options Additional connection options for the HttpConnector
+	 * @return \F3\CouchDB\Client
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
 	public function __construct($dataSourceName, $options = array()) {
 		if (($urlParts = @parse_url($dataSourceName)) === FALSE) {
 			throw new \F3\FLOW3\Exception('Invalid data source name: ' . $dataSourceName, 1287346792);
@@ -52,15 +60,20 @@ class Client {
 
 	/**
 	 * List all databases
+	 *
 	 * @return array
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	public function listDatabases() {
 		return $this->connector->get('/_all_dbs');
 	}
 
 	/**
+	 * Create a database
+	 *
 	 * @param string $database
 	 * @return boolean
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	public function createDatabase($database) {
 		$response = $this->connector->put('/' . urlencode($database));
@@ -68,8 +81,11 @@ class Client {
 	}
 
 	/**
+	 * Delete a database
+	 *
 	 * @param string $database
 	 * @return boolean
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	public function deleteDatabase($database) {
 		$response = $this->connector->delete('/' . urlencode($database));
@@ -77,48 +93,61 @@ class Client {
 	}
 
 	/**
-	 * @param string $database
+	 * Get information about a database
+	 *
+	 * @param string $database The database name
 	 * @return object
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	public function databaseInformation($database) {
 		return $this->connector->get('/' . urlencode($database));
 	}
 
 	/**
-	 * @param array $query
+	 * Get all documents in the database
+	 *
+	 * @param array $query Additional query options (e.g. limit or include_docs)
+	 * @return object
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	public function listDocuments($query = NULL) {
 		return $this->connector->get('/' . urlencode($this->getDatabase()) . '/_all_docs', $query);
 	}
 
 	/**
-	 * @param string id
-	 * @param array $query
+	 * Get a single document by id
+	 *
+	 * @param string id The document id
+	 * @param array $query Additional query options (e.g. revs or rev)
+	 * @return object
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	public function getDocument($id, $query = NULL) {
 		return $this->connector->get('/' . urlencode($this->getDatabase()) . '/' . $this->getEncodedId($id), $query);
 	}
 
-	protected function getEncodedId($id) {
-		if (strpos($id, '_design/') === 0) {
-			return '_design/' . urlencode(substr($id, strlen('_design/')));
-		} else {
-			return urlencode($id);
-		}
-	}
-
 	/**
-	 * @param array $keys
-	 * @param array $query
-	 */
-	public function getDocuments($keys, $query = NULL) {
-		return $this->connector->post('/' . urlencode($this->getDatabase()) . '/_all_docs', $query, json_encode(array('keys' => $keys)));
-	}
-
-	/**
+	 * Get multiple documents by id.
 	 *
-	 * @param mixed $idOrDocument Either the document id or the document itself as an array
+	 * Use include_docs => TRUE as query option to fetch the documents.
+	 *
+	 * @param array $ids The document ids as array
+	 * @param array $query Additional query options (e.g. include_docs)
+	 * @return object
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function getDocuments($ids, $query = NULL) {
+		return $this->connector->post('/' . urlencode($this->getDatabase()) . '/_all_docs', $query, json_encode(array('keys' => $ids)));
+	}
+
+	/**
+	 * Create a document either with a specified id, or by assigning a UUID
+	 * through CouchDB.
+	 *
+	 * @param mixed $idOrDocument Either the document id or the document itself as an string, array or object
 	 * @param array $documentOrNull The document or nothing if no id was given
+	 * @return \F3\CouchDB\Client\StatusResponse The creation response
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	public function createDocument($idOrDocument, $documentOrNull = NULL) {
 		if (is_string($idOrDocument)) {
@@ -135,9 +164,11 @@ class Client {
 	}
 
 	/**
+	 * Update a document
 	 *
-	 * @param string $idEither The document id
-	 * @param array $document The document
+	 * @param string $id The document id
+	 * @param mixed $document The document data as string or array / object
+	 * @return \F3\CouchDB\Client\StatusResponse The update response
 	 */
 	public function updateDocument($id, $document) {
 		if (!is_string($document)) {
@@ -147,9 +178,11 @@ class Client {
 	}
 
 	/**
+	 * Delete a document
 	 *
-	 * @param string $id
-	 * @param string $revision
+	 * @param string $id The document id
+	 * @param string $revision The document revision
+	 * @return boolean TRUE if the deletion was successful
 	 */
 	public function deleteDocument($id, $revision) {
 		$response = $this->connector->delete('/' . urlencode($this->getDatabase()) . '/' . $this->getEncodedId($id), array('rev' => $revision));
@@ -157,10 +190,16 @@ class Client {
 	}
 
 	/**
+	 * Query a view
+	 * 
+	 * In addition to the default view query options (key, startkey, endkey, ...)
+	 * the query parameter "keys" can be specified to do multi-key lookups.
 	 *
-	 * @param string $design
-	 * @param string $view
-	 * @param array $query
+	 * @param string $design The design document name
+	 * @param string $view The view name
+	 * @param array $query Query options
+	 * @return mixed
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
 	 */
 	public function queryView($design, $view, $query = NULL) {
 		$path = '/' . urlencode($this->getDatabase()) . '/_design/' . urlencode($design) . '/_view/' . urlencode($view);
@@ -174,17 +213,25 @@ class Client {
 	}
 
 	/**
+	 * Encode a document id and preserve slashes for design documents
+	 *
+	 * @param string $id
+	 * @return string
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	protected function getEncodedId($id) {
+		if (strpos($id, '_design/') === 0) {
+			return '_design/' . urlencode(substr($id, strlen('_design/')));
+		} else {
+			return urlencode($id);
+		}
+	}
+
+	/**
 	 * @return \F3\CouchDB\Client\HttpConnector
 	 */
 	public function getConnector() {
 		return $this->connector;
-	}
-
-	/**
-	 * @param string $database
-	 */
-	public function setDatabase($database) {
-		$this->database = $database;
 	}
 
 	/**
@@ -195,6 +242,13 @@ class Client {
 			throw new \F3\FLOW3\Exception('No database set', 1287349160);
 		}
 		return $this->database;
+	}
+
+	/**
+	 * @param string $database
+	 */
+	public function setDatabase($database) {
+		$this->database = $database;
 	}
 }
 
